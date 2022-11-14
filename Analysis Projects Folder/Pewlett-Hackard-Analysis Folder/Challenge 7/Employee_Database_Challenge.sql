03598938ff3c7cf16bd1d5b1e67869cf45bb551c
Mahbubur Rahman <mueeze@gmail.com>
2022-11-13 20:22:48 -0500

    Adding Pewlett Hackard Analysis

--- Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Challenge 7/Employee_Database_Challenge.sql
+++ Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Challenge 7/Employee_Database_Challenge.sql
@@ -42,8 +42,26 @@ order by e.emp_no;
 
 -- Deliverable 3
 
-SELECT count(me.title), me.title
--- INTO retiring_titles
+SELECT count(me.title), me.title 
 FROM mentorship_eligibility as me
 Group BY me.title 
+order by count DESC;
+
+select distinct on (e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date, de.from_date, de.to_date, t.title, d.dept_name
+into ment_dept
+from employees as e
+inner join dept_emp as de
+on (e.emp_no = de.emp_no)
+inner join titles as t
+on (e.emp_no = t.emp_no)
+inner join departments as d 
+on (d.dept_no = de.dept_no)
+where (de.to_date = '9999-01-01')
+and (e.birth_date between '1965-01-01' and '1965-12-31')
+order by e.emp_no;
+
+SELECT count(md.title), md.title, md.dept_name
+into ment_count_dept
+FROM ment_dept as md
+Group BY md.title, md.dept_name 
 order by count DESC;
